1d30170905e4569b7cc90a2a33baae6974ccbaa1
이름 <changukwoo@naver.com>
2021-04-05 15:18:37 +0900

    Create 1030.c

---
+++ CodeUp_C/Basic_100/1026.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c;
+
+	scanf("%d:%d:%d", &a, &b, &c);
+
+	printf("%d\n", b);
+
+	return 0;
+}
---
+++ CodeUp_C/Basic_100/1027.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c;
+
+	scanf("%d.%d.%d", &a, &b, &c);
+
+	printf("%02d-%02d-%04d\n", c, b, a);
+
+	return 0;
+}
